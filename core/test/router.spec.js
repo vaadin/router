02d43c9301d6edecde4894a7d0dfa330bcb1ff79
Viktor Lukashov <viktor@vaadin.com>
2018-04-10 10:24:51 +0300

    add a few tests into the vaadin-router-core package
     - run all tests in all local browsers automatically: `npm run test` / `yarn test` (in `/core`)
     - run all / some of the tests manually: `npm run start` / `yarn start` (in `/core`), then open `http://localhost:8000/components/vaadin-router-core/test/`
     - use the Web Component Tester library to run tests in browsers (same approach to testing as for Vaadin components / Polymer elements)
     - create a sample JS test suite (does not interact with the DOM)
     - create a sample HTML test suite (interacts with the DOM) - this one would run both with and without the Shady DOM polyfil

--- core/test/router.spec.js
+++ core/test/router.spec.js
@@ -1,9 +1,38 @@
-const expect = require('chai').expect;
-const Router = require('../dist/vaadin-router-core');
-
 describe('Vaadin.Router', () => {
-  it('should have a no-arg constructor', () => {
-    const router = new Router();
-    expect(router).to.not.be.null;
+  describe('router outlet (basic functionality)', () => {
+
+    let router;
+    let routes;
+    beforeEach(() => {
+      router = new Vaadin.Router();
+      routes = [
+        {path: '/', component: 'x-home-view'},
+        {path: '/users', component: 'x-users-view'}
+      ];
+    });
+
+    it('should have a no-arg constructor', () => {
+      expect(router).to.be.ok;
+    });
+
+    it('should have a setter for the routes config', () => {
+      router.setRoutes([
+        {path: '/', component: 'x-home-view'},
+        {path: '/users', component: 'x-users-view'}
+      ]);
+      expect(router).to.be.ok;
+    });
+
+    it('should have a getter for the routes config', () => {
+      router.setRoutes(routes);
+      const actual = router.getRoutes();
+      expect(actual).to.be.eql(routes);
+    });
+
+    it('should have a constructor accepting routes', () => {
+      const router = new Vaadin.Router(routes);
+      const actual = router.getRoutes();
+      expect(actual).to.be.eql(routes);
+    });
   });
 });
---
+++ core/test/test-suites.js
@@ -0,0 +1,4 @@
+window.VaadinRouterCoreSuites = [
+  'router.spec.js',
+  'router-dom.spec.html',
+];
