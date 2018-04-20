9ab4a7a3f7075884c5693beb8489029556647481
Viktor Lukashov <viktor@vaadin.com>
2018-04-20 07:06:40 +0300

    fix the matchRoute.spec to run with WCT
     - the .nested.property assertion used to be .deep.property before chai 4.x. WCT uses chai 3.5, thus the old syntax.

--- core/test/resolver/matchRoute.spec.js
+++ core/test/resolver/matchRoute.spec.js
@@ -24,6 +24,7 @@ describe('matchRoute(route, baseUrl, pathname)', () => {
   it('should match 0 routes (1)', () => {
     const route = {
       path: '/',
+      exact: true,
     }
     const result = toArray(matchRoute(route, '', '/a'))
     expect(result).to.have.lengthOf(0)
@@ -71,7 +72,7 @@ describe('matchRoute(route, baseUrl, pathname)', () => {
     expect(result).to.have.lengthOf(1)
     expect(result[0]).to.have.property('baseUrl', '')
     expect(result[0]).to.have.property('path', '/')
-    expect(result[0]).to.have.nested.property('route.path', '/')
+    expect(result[0]).to.have.deep.property('route.path', '/')
   })
 
   it('should match 1 route (2)', () => {
@@ -82,7 +83,7 @@ describe('matchRoute(route, baseUrl, pathname)', () => {
     expect(result).to.have.lengthOf(1)
     expect(result[0]).to.have.property('baseUrl', '')
     expect(result[0]).to.have.property('path', '/a')
-    expect(result[0]).to.have.nested.property('route.path', '/a')
+    expect(result[0]).to.have.deep.property('route.path', '/a')
   })
 
   it('should match 2 routes (1)', () => {
@@ -98,10 +99,10 @@ describe('matchRoute(route, baseUrl, pathname)', () => {
     expect(result).to.have.lengthOf(2)
     expect(result[0]).to.have.property('baseUrl', '')
     expect(result[0]).to.have.property('path', '')
-    expect(result[0]).to.have.nested.property('route.path', '')
+    expect(result[0]).to.have.deep.property('route.path', '')
     expect(result[1]).to.have.property('baseUrl', '')
     expect(result[1]).to.have.property('path', '/a')
-    expect(result[1]).to.have.nested.property('route.path', '/a')
+    expect(result[1]).to.have.deep.property('route.path', '/a')
   })
 
   it('should match 2 routes (2)', () => {
@@ -121,11 +122,11 @@ describe('matchRoute(route, baseUrl, pathname)', () => {
     const result = toArray(matchRoute(route, '', '/a/b/c', []))
     expect(result).to.have.lengthOf(3)
     expect(result[0]).to.have.property('baseUrl', '')
-    expect(result[0]).to.have.nested.property('route.path', '/a')
+    expect(result[0]).to.have.deep.property('route.path', '/a')
     expect(result[1]).to.have.property('baseUrl', '/a')
-    expect(result[1]).to.have.nested.property('route.path', '/b')
+    expect(result[1]).to.have.deep.property('route.path', '/b')
     expect(result[2]).to.have.property('baseUrl', '/a/b')
-    expect(result[2]).to.have.nested.property('route.path', '/c')
+    expect(result[2]).to.have.deep.property('route.path', '/c')
   })
 
   it('should match 2 routes (3)', () => {
@@ -140,9 +141,9 @@ describe('matchRoute(route, baseUrl, pathname)', () => {
     const result = toArray(matchRoute(route, '', '/', []))
     expect(result).to.have.lengthOf(2)
     expect(result[0]).to.have.property('baseUrl', '')
-    expect(result[0]).to.have.nested.property('route.path', '')
+    expect(result[0]).to.have.deep.property('route.path', '')
     expect(result[1]).to.have.property('baseUrl', '')
-    expect(result[1]).to.have.nested.property('route.path', '')
+    expect(result[1]).to.have.deep.property('route.path', '')
   })
 })
 })(ResolverTest);
