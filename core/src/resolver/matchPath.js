bc11952e9afb5a86532af1e57f1f3b009719a796
Viktor Lukashov <viktor@vaadin.com>
2018-04-25 17:46:11 +0300

    reformat the universal-router sources with the vaadin eslint config
     - run `eslint --fix`

--- core/src/resolver/matchPath.js
+++ core/src/resolver/matchPath.js
@@ -7,57 +7,57 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import pathToRegexp from './path-to-regexp.js'
+import pathToRegexp from './path-to-regexp.js';
 
-const { hasOwnProperty } = Object.prototype
-const cache = new Map()
+const {hasOwnProperty} = Object.prototype;
+const cache = new Map();
 // see https://github.com/pillarjs/path-to-regexp/issues/148
 cache.set('|false', {
   keys: [],
   pattern: /(?:)/
-})
+});
 
 function decodeParam(val) {
   try {
-    return decodeURIComponent(val)
+    return decodeURIComponent(val);
   } catch (err) {
-    return val
+    return val;
   }
 }
 
 function matchPath(routepath, path, exact, parentKeys, parentParams) {
-  exact = !!exact
-  const cacheKey = `${routepath}|${exact}`
-  let regexp = cache.get(cacheKey)
+  exact = !!exact;
+  const cacheKey = `${routepath}|${exact}`;
+  let regexp = cache.get(cacheKey);
 
   if (!regexp) {
-    const keys = []
+    const keys = [];
     regexp = {
       keys,
       pattern: pathToRegexp(routepath, keys, {
         end: exact,
         strict: routepath === ''
       }),
-    }
-    cache.set(cacheKey, regexp)
+    };
+    cache.set(cacheKey, regexp);
   }
 
-  const m = regexp.pattern.exec(path)
+  const m = regexp.pattern.exec(path);
   if (!m) {
-    return null
+    return null;
   }
 
-  const params = Object.assign({}, parentParams)
+  const params = Object.assign({}, parentParams);
 
   for (let i = 1; i < m.length; i++) {
-    const key = regexp.keys[i - 1]
-    const prop = key.name
-    const value = m[i]
+    const key = regexp.keys[i - 1];
+    const prop = key.name;
+    const value = m[i];
     if (value !== undefined || !hasOwnProperty.call(params, prop)) {
       if (key.repeat) {
-        params[prop] = value ? value.split(key.delimiter).map(decodeParam) : []
+        params[prop] = value ? value.split(key.delimiter).map(decodeParam) : [];
       } else {
-        params[prop] = value ? decodeParam(value) : value
+        params[prop] = value ? decodeParam(value) : value;
       }
     }
   }
@@ -66,7 +66,7 @@ function matchPath(routepath, path, exact, parentKeys, parentParams) {
     path: m[0],
     keys: (parentKeys || []).concat(regexp.keys),
     params,
-  }
+  };
 }
 
-export default matchPath
+export default matchPath;
--- core/src/resolver/matchRoute.js
+++ core/src/resolver/matchRoute.js
@@ -7,25 +7,25 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import matchPath from './matchPath.js'
+import matchPath from './matchPath.js';
 
 function matchRoute(route, pathname, parentKeys, parentParams) {
-  let match
-  let childMatches
-  let childIndex = 0
-  let routepath = route.path || ''
+  let match;
+  let childMatches;
+  let childIndex = 0;
+  let routepath = route.path || '';
   if (route.parent && routepath.charAt(0) === '/') {
-    routepath = routepath.substr(1, routepath.length - 1)
+    routepath = routepath.substr(1, routepath.length - 1);
   } 
 
   return {
     next(routeToSkip) {
       if (route === routeToSkip) {
-        return { done: true }
+        return {done: true};
       }
 
       if (!match) {
-        match = matchPath(routepath, pathname, route.exact, parentKeys, parentParams)
+        match = matchPath(routepath, pathname, route.exact, parentKeys, parentParams);
 
         if (match) {
           return {
@@ -35,49 +35,48 @@ function matchRoute(route, pathname, parentKeys, parentParams) {
               keys: match.keys,
               params: match.params,
             },
-          }
-        }
-        else if (route.exact) {
-          match = matchPath(routepath, pathname, false, parentKeys, parentParams)
+          };
+        } else if (route.exact) {
+          match = matchPath(routepath, pathname, false, parentKeys, parentParams);
         }
       }
 
       if (match && route.children) {        
         while (childIndex < route.children.length) {
           if (!childMatches) {
-            const childRoute = route.children[childIndex]
-            childRoute.parent = route
+            const childRoute = route.children[childIndex];
+            childRoute.parent = route;
 
             let matchedPath = match.path;
             if (matchedPath.charAt(matchedPath.length - 1) !== '/'
                 && !(route.path === '' && (childRoute.path || '').charAt(0) !== '/')) {
-              matchedPath += '/'
+              matchedPath += '/';
             }
 
             childMatches = matchRoute(
               childRoute,
               pathname.substr(matchedPath.length),
               match.keys,
-              match.params,
-            )
+              match.params
+            );
           }
 
-          const childMatch = childMatches.next(routeToSkip)
+          const childMatch = childMatches.next(routeToSkip);
           if (!childMatch.done) {
             return {
               done: false,
               value: childMatch.value,
-            }
+            };
           }
 
-          childMatches = null
-          childIndex++
+          childMatches = null;
+          childIndex++;
         }
       }
 
-      return { done: true }
+      return {done: true};
     },
-  }
+  };
 }
 
-export default matchRoute
+export default matchRoute;
