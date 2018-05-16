668df925d1915fc6ff599086505f66792b1861e8
Viktor Lukashov <viktor@vaadin.com>
2018-05-16 15:26:29 +0300

    lift the contents of /core up into the root
     - remove all vaadin-router-* custom elements (they are kept in the `vaadin-router-element` branch)

--- wct.conf.js
+++ wct.conf.js
@@ -1,51 +1,35 @@
 var argv = require('yargs').argv;
 
+// This file is here to mark the project root for WCT.
+// (so that when started in this directory it runs for vaadin-router-core, not for vaadin-router)
 module.exports = {
   registerHooks: function(context) {
-    var saucelabsPlatforms = [
-      'macOS 10.12/iphone@10.3',
-      'macOS 10.12/ipad@11.0',
-      'Windows 10/microsoftedge@15',
-      'Windows 10/internet explorer@11',
-      'macOS 10.12/safari@11.0',
-      'macOS 9.3.2/iphone@9.3'
-    ];
-
-    var cronPlatforms = [
-      'Android/chrome',
-      'Windows 10/chrome@59',
-      'Windows 10/firefox@54'
-    ];
-
-    if (argv.env === 'saucelabs') {
-      context.options.plugins.sauce.browsers = saucelabsPlatforms;
-
-    } else if (argv.env === 'saucelabs-cron') {
-      context.options.plugins.sauce.browsers = cronPlatforms;
-    }
-
     if (argv.profile === 'coverage') {
+      context.options.suites = [
+        'test/index.coverage.html'
+      ];
+
       context.options.plugins.local.browsers = ['chrome'];
 
       context.options.plugins.istanbub = {
         dir: './coverage',
         reporters: ['text-summary', 'lcov'],
         include: [
-          '**/*.html',
+          '**/dist/test-iife/**/*.js',
         ],
         exclude: [
-          '**/dist/**/*.js'
+          '**/dist/test-iife/resolver/path-to-regexp.js',
+          '**/dist/test-iife/resolver/generateUrls.js',
         ],
-        // TODO(vlukashov): set to 80% when start working on <vaadin-router>
         thresholds: {
           global: {
-            statements: 50,
-            branches: 50,
-            functions: 50,
-            lines: 50,
+            statements: 80,
+            branches: 80,
+            functions: 80,
+            lines: 80,
           }
         }
       };
     }
-  },
+  }
 };
