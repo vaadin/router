fc572124cf0d32c39f131852979e8fb864e25ee7
web-padawan <serguey.kulikov@gmail.com>
2018-06-13 12:06:21 +0300

    Update DOM manipulations order for animations support

--- src/router.js
+++ src/router.js
@@ -261,14 +261,38 @@ export class Router extends Resolver {
           }
 
           if (context.route !== (this.__previousContext || {}).route) {
+            this.__saveOldOutletContent();
             this.__setOutletContent(context.result);
-            const currentComponent = context.route.__component || {};
-            return Promise.resolve(runCallbackIfPossible(currentComponent.onAfterEnter,
-              Object.assign({}, context, {next: undefined}), currentComponent))
-              .then(() => context);
           }
           return Promise.resolve(context);
         }
+      })
+      .then(context => {
+        return new Promise(resolve => {
+          const from = this.__oldContent && this.__oldContent[0];
+          const to = this.__newContent;
+          if (from && to && context.route && typeof context.route.animate === 'function') {
+            Promise.resolve(context.route.animate(from, to)).then(() => {
+              this.__removeOldOutletContent();
+              resolve(context);
+            });
+          } else {
+            this.__removeOldOutletContent();
+            resolve(context);
+          }
+        });
+      })
+      .then((context) => {
+        if (this.__newContent) {
+          const currentComponent = context.route.__component || {};
+          return Promise.resolve(runCallbackIfPossible(currentComponent.onAfterEnter,
+            Object.assign({}, context, {next: undefined}), currentComponent))
+            .then(() => {
+              this.__newContent = null;
+              return context;
+            });
+        }
+        return Promise.resolve(context);
       })
       .then(context => {
         this.__previousContext = context;
@@ -279,7 +303,7 @@ export class Router extends Resolver {
           if (shouldUpdateHistory) {
             this.__updateBrowserHistory(pathnameOrContext);
           }
-          this.__setOutletContent();
+          this.__removeOutletContent();
           throw error;
         }
       });
@@ -344,17 +368,34 @@ export class Router extends Resolver {
     }
   }
 
+  __saveOldOutletContent() {
+    this.__ensureOutlet();
+    this.__oldContent = Array.from(this.__outlet.children);
+  }
+
   __setOutletContent(element) {
     this.__ensureOutlet();
-    const children = this.__outlet.children;
-    if (children && children.length) {
-      const parent = children[0].parentNode;
-      for (let i = 0; i < children.length; i += 1) {
-        parent.removeChild(children[i]);
-      }
-    }
     if (element) {
       this.__outlet.appendChild(element);
+      this.__newContent = element;
+    }
+  }
+
+  __removeOldOutletContent() {
+    if (this.__oldContent) {
+      this.__removeOutletContent(this.__oldContent);
+      this.__oldContent = null;
+    }
+  }
+
+  __removeOutletContent(content) {
+    this.__ensureOutlet();
+    content = content || this.__outlet.children;
+    if (content && content.length) {
+      const parent = content[0].parentNode;
+      for (let i = 0; i < content.length; i += 1) {
+        parent.removeChild(content[i]);
+      }
     }
   }
 
