c4cd39facca75825fa114bdbe28470f21b7b3a81
Kirill Bulatov <kirill@vaadin.com>
2018-06-13 14:35:37 +0300

    Remove redundant logging.

--- test/router/test-utils.js
+++ test/router/test-utils.js
@@ -2,6 +2,4 @@ function verifyActiveRoutes(router, expectedSegments) {
   expect(router.__previousContext.chain.map(route => route.path)).to.deep.equal(expectedSegments);
 }
 
-console.log('!1!!!!');
-
 (window.VaadinTestNamespace || (window.VaadinTestNamespace = {})).verifyActiveRoutes = verifyActiveRoutes;
