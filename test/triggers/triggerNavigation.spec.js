b901ba59e5a213fa8ec2fedba58e7eee9c771731
Viktor Lukashov <viktor@vaadin.com>
2018-06-19 10:37:40 +0300

    add a static Router.go(path) method
     - rename the `vaadin-router:navigate` event to `vaadin-router:go` to match the method name.
     - add a test and a demo

--- test/triggers/triggerNavigation.spec.js
+++ test/triggers/triggerNavigation.spec.js
@@ -1,19 +1,19 @@
 (({triggerNavigation}) => {
   describe('triggerNavigation', () => {
-    it('should trigger a `vaadin-router:navigate` event on the `window`', () => {
+    it('should trigger a `vaadin-router:go` event on the `window`', () => {
       const spy = sinon.spy();
-      window.addEventListener('vaadin-router:navigate', spy);
+      window.addEventListener('vaadin-router:go', spy);
       triggerNavigation('');
-      window.removeEventListener('vaadin-router:navigate', spy);
+      window.removeEventListener('vaadin-router:go', spy);
       expect(spy).to.have.been.called.once;
-      expect(spy.args[0][0]).to.have.property('type', 'vaadin-router:navigate');
+      expect(spy.args[0][0]).to.have.property('type', 'vaadin-router:go');
     });
 
     it('should pass the given pathname in the `detail.pathname` property of the triggered event', () => {
       const spy = sinon.spy();
-      window.addEventListener('vaadin-router:navigate', spy);
+      window.addEventListener('vaadin-router:go', spy);
       triggerNavigation('/a');
-      window.removeEventListener('vaadin-router:navigate', spy);
+      window.removeEventListener('vaadin-router:go', spy);
       expect(spy).to.have.been.called.once;
       expect(spy.args[0][0]).to.have.deep.property('detail.pathname', '/a');
     });
