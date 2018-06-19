dd3f1b0df226f2d45a4c3a67742b940a9787b05b
Kirill Bulatov <mail4score@gmail.com>
2018-06-19 10:18:51 +0300

    Log difference in lifecycle callbacks. (#179)

--- src/triggers/triggerNavigation.js
+++ src/triggers/triggerNavigation.js
@@ -2,7 +2,7 @@
  * Creates and dispatches a new `vaadin-router:navigate` event on the given
  * context (`window` by default). That triggers Vaadin.Router navigation to the
  * given path.
- * 
+ *
  * @param {!string} pathname a new pathname
  */
 export default function triggerNavigation(pathname) {
