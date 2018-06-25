89e755cbe6bb79b5c9c8a8e8e7d7b5c16ae6ea1b
web-padawan <serguey.kulikov@gmail.com>
2018-06-25 15:40:12 +0300

    Include development-mode-detector in ES module bundle

--- rollup.config.js
+++ rollup.config.js
@@ -39,13 +39,14 @@ const config = [
       file: pkg.browser,
       sourcemap: true,
     },
-    plugins
+    plugins,
+    external: id => /@vaadin/.test(id)
   },
 
   // UMD bundle, transpiled (for the browsers that do not support ES modules).
   // Also works in Node.
   {
-    input: 'index.js',
+    input: 'index.umd.js',
     output: {
       format: 'umd',
       file: pkg.main,
