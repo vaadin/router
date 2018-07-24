7d781e70dda725a23a2337819f9c32b2232b4123
Limon Monte <limon.monte@gmail.com>
2018-07-24 16:09:17 +0900

    Use 'latest' and 'latest-1' for SauceLabs platforms (#240)
    > Default to Latest Version of Chrome or Firefox
    > If you want to use the latest stable version of Google Chrome or Firefox that Sauce supports, you can use "version": "latest". You can also use "version": "latest-1" or "version": "latest-2", etc.

--- wct.conf.js
+++ wct.conf.js
@@ -6,8 +6,8 @@ module.exports = {
       // The list below is based on the browserslist config defined in package.json
       context.options.plugins.sauce.browsers = [
         // // last 2 Chrome major versions (desktop)
-        'Windows 10/chrome@66',
-        'Windows 10/chrome@67',
+        'Windows 10/chrome@latest',
+        'Windows 10/chrome@latest-1',
 
         // last 2 Android major versions (mobile Chrome)
         {
@@ -15,23 +15,23 @@ module.exports = {
           platformName: 'Android',
           platformVersion: '7.1',
           browserName: 'chrome',
-          browserVersion: '67'
+          browserVersion: 'latest'
         },
         {
           deviceName: 'Android Emulator',
           platformName: 'Android',
           platformVersion: '6.0',
           browserName: 'chrome',
-          browserVersion: '66'
+          browserVersion: 'latest-1'
         },
 
         // last 2 Firefox major versions (desktop)
-        'Windows 10/firefox@59',
-        'Windows 10/firefox@60',
+        'Windows 10/firefox@latest',
+        'Windows 10/firefox@latest-1',
 
         // last 2 Edge major versions (desktop)
-        'Windows 10/microsoftedge@16',
-        'Windows 10/microsoftedge@17',
+        'Windows 10/microsoftedge@latest',
+        'Windows 10/microsoftedge@latest-1',
 
         // last 2 Safari major versions (desktop)
         'macOS 10.13/safari@11.1',
