db0ed1a7d42712470d974efddb0b9eebac23e250
Viktor Lukashov <viktor@vaadin.com>
2018-10-30 16:37:06 +0200

    1.2.0-pre.3

--- src/router-meta.js
+++ src/router-meta.js
@@ -5,7 +5,7 @@ window.Vaadin.registrations = window.Vaadin.registrations || [];
 
 window.Vaadin.registrations.push({
   is: '@vaadin/router',
-  version: '1.2.0-pre.2',
+  version: '1.2.0-pre.3',
 });
 
 usageStatistics();
