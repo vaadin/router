2723045ba6c1e711ff3c3f2dd01b5e67279b67e7
Anton Platonov <platosha@gmail.com>
2019-08-09 14:10:16 +0300

    chore: update yarn.lock, fixes `yarn docs` with Node.js v11 (#306)
    * chore: update yarn.lock

--- docs/vaadin-router/demo/demo-elements/users-routes.js
+++ docs/vaadin-router/demo/demo-elements/users-routes.js
@@ -1,1 +1,1 @@
-(function(){window.Vaadin=window.Vaadin||{};Vaadin.Demo=Vaadin.Demo||{};Vaadin.Demo.moduleStorage=Vaadin.Demo.moduleStorage||[];Vaadin.Demo.moduleStorage.push({default:[{path:"/",component:"x-user-home"},{path:"/:user",component:"x-user-profile"}]})})();
+(function(){window.Vaadin=window.Vaadin||{};Vaadin.Demo=Vaadin.Demo||{};Vaadin.Demo.moduleStorage=Vaadin.Demo.moduleStorage||[];var usersRoutes=[{path:"/",component:"x-user-home"},{path:"/:user",component:"x-user-profile"}];Vaadin.Demo.moduleStorage.push({default:usersRoutes})})();
