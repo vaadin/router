100cc05d8a3d7e63e93ad24025614c6da22aa6cc
Abdón Rodríguez Davila <a@abdonrd.com>
2019-08-13 14:22:24 +0200

    Simplify rollup babel config

--- rollup.config.js
+++ rollup.config.js
@@ -56,27 +56,6 @@ const config = [
     plugins: [
       ...plugins,
       babel({
-        // The 'external-helpers' Babel plugin allows Rollup to include every
-        // used babel helper just once per bundle, rather than including them in
-        // every module that uses them (which is the default behaviour).
-        plugins: ['external-helpers'],
-        presets: [
-          ['env', {
-            // Run `npm run browserslist` to see the percent of users covered
-            // by this browsers selection
-            targets: {
-              browsers: pkg.browserslist
-            },
-
-            // Instructs Babel to not convert ES modules to CommonJS--that's a
-            // job for Rollup.
-            modules: false,
-
-            // To see which browsers are targeted, and which JS features are
-            // polyfilled, uncomment the next line and run `npm run build`
-            // debug: true,
-          }]
-        ],
       })
     ]
   },
