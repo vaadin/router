969a3dbc2c328b74623edbfb05f39b9ee7888ae5
Viktor Lukashov <viktor@vaadin.com>
2019-09-02 14:23:08 +0300

    1.4.1

--- src/router-meta.js
+++ src/router-meta.js
@@ -5,7 +5,7 @@ window.Vaadin.registrations = window.Vaadin.registrations || [];
 
 window.Vaadin.registrations.push({
   is: '@vaadin/router',
-  version: '1.4.0',
+  version: '1.4.1',
 });
 
 usageStatistics();
