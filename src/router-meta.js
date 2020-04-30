397d8a706c40b91a2b76179e1416520e11146491
haijian <haijian@vaadin.com>
2020-04-30 10:58:40 +0300

    1.7.2

--- src/router-meta.js
+++ src/router-meta.js
@@ -5,7 +5,7 @@ window.Vaadin.registrations = window.Vaadin.registrations || [];
 
 window.Vaadin.registrations.push({
   is: '@vaadin/router',
-  version: '1.7.1',
+  version: '1.7.2',
 });
 
 usageStatistics();
