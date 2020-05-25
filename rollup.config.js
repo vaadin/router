e1327564e2b2a61de134d735059970916c00f242
Abdón Rodríguez Davila <a@abdonrd.com>
2020-05-25 16:22:19 +0200

    Update Rollup dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import path from 'path';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
 import pkg from './package.json';
 
 const plugins = [
