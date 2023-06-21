39bd3c7c3238b78b6be20865655e52a5394c0e56
Vlad Rindevich <vladrin@vaadin.com>
2023-06-21 18:40:22 +0300

    chore: update config files

--- karma.config.cjs
+++ karma.config.cjs
@@ -12,16 +12,9 @@ const coverage = !!process.argv.find((arg) => arg.includes('--coverage'));
 
 module.exports = (config) => {
   config.set({
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
     plugins: [karmaMocha, karmaChromeLauncher, karmaVite, karmaCoverage],
 
     browsers: ['ChromeHeadlessNoSandbox'],
-    browserNoActivityTimeout: 60000, // default 10000
-    browserDisconnectTimeout: 10000, // default 2000
-    browserDisconnectTolerance: 1, // default 0
-    captureTimeout: 60000,
 
     // you can define custom flags
     customLaunchers: {
@@ -31,11 +24,8 @@ module.exports = (config) => {
       },
     },
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['vite', 'mocha'],
 
-    // list of files / patterns to load in the browser
     files: [
       {
         pattern: 'test/**/*.spec.ts',
@@ -51,34 +41,10 @@ module.exports = (config) => {
       },
     ],
 
-    // list of files / patterns to exclude
-    exclude: [],
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', coverage && 'coverage'].filter(Boolean),
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-    // web server port
-    port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: watch,
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: !watch,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity,
 
     coverageReporter: {
       dir: '.coverage/',
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
@@ -6,7 +5,5 @@ export default defineConfig({
   build: {
     target: 'esnext',
   },
-  root: resolve(
-    process.cwd()
-  ),
+  root: process.cwd(),
 });
