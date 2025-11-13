f9f6ff6b0c2e3d268c4c743f445093f859da48e5
Anton Platonov <platosha@gmail.com>
2025-11-13 14:57:18 +0200

    docs: deprecation notice (#916)
    * docs: deprecation notice
    
    * docs: apply review suggestions

--- src/router.ts
+++ src/router.ts
@@ -97,6 +97,10 @@ const rootContext: RouteContext = {
  * * [setRoutes](#/classes/Router#method-setRoutes) – should be used to configure the routes.
  *
  * Only `setRoutes` has to be called manually, others are automatically invoked when creating a new instance.
+ *
+ * @deprecated This library is no longer actively maintained. Vaadin uses React Router as a primary client-side routing tool.
+ *
+ * Consider [URLPattern](https://developer.mozilla.org/en-US/docs/Web/API/URLPattern) standard Web API or a library based on it, such as [@lit-labs/router](https://www.npmjs.com/package/@lit-labs/router)
  */
 export class Router<R extends object = EmptyObject, C extends object = EmptyObject> extends Resolver<
   ActionValue,
